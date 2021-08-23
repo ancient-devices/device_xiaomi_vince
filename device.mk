7dff55bd0120667e2d07323a691dbedc06d7e5c1
argraur <argraur@gmail.com>
2021-08-23 22:56:38 +0530

    vince: Use Pixel charger animation
    Signed-off-by: Ray <vincentrayananda@gmail.com>
    Signed-off-by: Edwiin kusuma jaya <kutemeikito0905@gmail.com>

--- device.mk
+++ device.mk
@@ -348,6 +348,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     GCamGo
 
+# Offline charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
