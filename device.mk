504022635f6c02b73fe4fe19c82cecd008ad5786
GhostMaster69-dev <rathore6375@gmail.com>
2022-01-10 20:18:51 +0530

    vince: Provide various HAL libs on vendor
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: Edwiin kusuma jaya <kutemeikito0905@gmail.com>

--- device.mk
+++ device.mk
@@ -193,6 +193,7 @@ PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
     libqdMetaData \
     libtinyxml \
+    libtinyxml.vendor \
     libvulkan \
     vendor.display.config@1.0.vendor \
     vendor.display.config@2.0
@@ -336,6 +337,10 @@ PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
     libnbaio
 
+# Netmgrd
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.1.vendor
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
@@ -397,13 +402,23 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.secure_element@1.0 \
+    android.hardware.radio@1.4.vendor \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio@1.6.vendor \
+    android.hardware.radio.config@1.1.vendor \
+    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio.config@1.3.vendor \
+    android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.secure_element@1.0.vendor \
+    android.hardware.secure_element@1.1.vendor \
+    android.hardware.secure_element@1.2.vendor \
     extphonelib \
     extphonelib.xml \
     extphonelib-product \
     extphonelib_product.xml \
-    librmnetctl \
     libcnefeatureconfig \
+    librmnetctl \
+    librmnetctl.vendor \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-hidl-wrapper-prd \
@@ -412,20 +427,7 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     qti-telephony-utils-prd \
     qti-telephony-utils_prd.xml \
-    telephony-ext \
-    android.system.net.netd@1.1.vendor \
-    android.hardware.radio@1.5.vendor \
-    android.hardware.radio@1.4.vendor \
-    android.hardware.radio.config@1.2.vendor \
-    android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.secure_element@1.2.vendor \
-    android.hardware.secure_element@1.1.vendor \
-    android.hardware.secure_element@1.0.vendor \
-    librmnetctl \
-    librmnetctl.vendor \
-    libtinyxml.vendor
-
-
+    telephony-ext
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
