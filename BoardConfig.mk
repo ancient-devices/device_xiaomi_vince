57091f75a02303d6d85992430f243c955358e322
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-03-02 12:16:26 -0600

    vince: add private sepolicy dir
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>
    Change-Id: I2ce223c396d42b6ab830ec23bda902425ce166ac
    Signed-off-by: Edwiin kusuma jaya <kutemeikito0905@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -211,6 +211,7 @@ VENDOR_SECURITY_PATCH := 2021-07-05
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # SurfaceFlinger
 TARGET_USE_AOSP_SURFACEFLINGER := true
